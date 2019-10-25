9660d2240f3bd26c6e9726203262300c83544437
DESARROLLOSNEA\jose.fernandez <jfernandez@desarrollosnea.com>
2019-10-25 11:36:01 -0300

    Plantilla de Inicio de Sitio (Home)

---
+++ src/Poncho/Poncho/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Poncho.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

2c52a5d2e9d71ebe031f507dd9ecee54b122ce59
DESARROLLOSNEA\jose.fernandez <jfernandez@desarrollosnea.com>
2019-10-25 14:38:37 -0300

    Template de Servicio

--- src/Poncho/Controllers/HomeController.cs
+++ src/Poncho/Controllers/HomeController.cs
@@ -43,6 +43,11 @@ namespace Poncho.Controllers
             return View();
         }
 
+        public IActionResult Servicio()
+        {
+            return View();
+        }
+
 
         public IActionResult Privacy()
         {
